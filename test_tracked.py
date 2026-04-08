7cdc6ea0840036ab5b4eeec923f490c8543968a9
Bramastya Fadhil Rinanto <bramastyafr@staff.uns.ac.id>
2026-04-08 10:33:19 +0700

    chore: remove obsolete test scripts, data files, and debug artifacts

--- test_tracked.py
+++
@@ -1,4 +0,0 @@
-import urllib.request
-req = urllib.request.Request('https://www.flightradar24.com/', headers={'User-Agent': 'Mozilla/5.0'})
-html=urllib.request.urlopen(req).read().decode('utf-8')
-print('DAL346' in html or 'most-tracked' in html or 'svg' in html)
